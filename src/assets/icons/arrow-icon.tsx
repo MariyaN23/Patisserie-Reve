b1600e75eac8f4c26808ce4d75a5e7f1ea883343
MariyaN23 <sellaite505@gmail.com>
2025-04-06 11:43:59 +0300

    add arrows and animations for mobile menu

---
+++ src/assets/icons/arrow-icon.tsx
@@ -0,0 +1,18 @@
+export const ArrowIcon = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={8}
+            height={14}
+            fill="none"
+        >
+            <path
+                stroke="#F79489"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.5}
+                d="M1.5 1.167 6.5 7l-5 5.833"
+            />
+        </svg>
+    )
+}
--- src/components/main/MenuItemsMobile.tsx
+++ src/components/main/MenuItemsMobile.tsx
@@ -2,6 +2,7 @@ import {MenuItem} from "../../utils/types.ts";
 import {useState} from "react";
 import {paths} from "../../utils/paths.ts";
 import {menuItems} from "../../utils/items.ts";
+import {ArrowIcon} from "../../assets/icons/arrow-icon.tsx";
 
 export const MenuItemsMobile = () => {
     const menuTitles = {
@@ -24,19 +25,18 @@ export const MenuItemsMobile = () => {
 }
 
 const MenuItemComponent = ({item, depth = 0}: { item: MenuItem, depth?: number }) => {
-    const [isOpen, setIsOpen] = useState(depth === 1)
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <li className={`${depth > 0 && 'pl-4 border-l-2 border-cream'}`}>
             <div className={`flex items-center ${depth === 0 ? 'font-bold text-xl' : 'font-medium'}`}
                  onClick={() => item.children && setIsOpen(!isOpen)}>
                 {item.children && (
-                    <span className={'mr-2'}>
-                        {isOpen ? '▼' : '►'}
+                    <span className={`mr-2 inline-block transition-transform duration-300 ${isOpen && 'rotate-90'}`}>
+                        <ArrowIcon/>
                     </span>)}
                 {item.title}
             </div>
-
             {item.image && (
                 <img
                     src={item.image}
@@ -44,17 +44,19 @@ const MenuItemComponent = ({item, depth = 0}: { item: MenuItem, depth?: number }
                     className={`menu-image w-48 h-48 object-cover my-2 ${depth > 0 && 'ml-6'}`}
                 />
             )}
-
-            {item.children && isOpen && (
-                <ul className={'mt-2 space-y-2'}>
-                    {item.children.map((child, index) => (
-                        <MenuItemComponent
-                            key={index}
-                            item={child}
-                            depth={depth + 1}
-                        />
-                    ))}
-                </ul>
+            {item.children && (
+                <div
+                    className={`opacity-0 transition-all duration-300 ease-in-out ${isOpen ? 'opacity-100 max-h-full' : 'max-h-0'}`}>
+                    <ul className={'mt-2 space-y-2'}>
+                        {item.children.map((child, index) => (
+                            <MenuItemComponent
+                                key={index}
+                                item={child}
+                                depth={depth + 1}
+                            />
+                        ))}
+                    </ul>
+                </div>
             )}
         </li>
     )
